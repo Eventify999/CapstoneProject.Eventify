d0e44d7424aa84a77c50281bdf073d2cb17409b9
anshbhandary <anshbhandary2002@gamil.com>
2025-04-09 10:17:49 +0530

    first commit

---
+++ VendorAPI/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VendorAPI.Models;
+using VendorAPI.Models.dto;
+using VendorAPI.Repository;
+using BCrypt.Net;
+
+namespace VendorAPI.Controllers
+{
+    [Route("api/vendor/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IVendorRepository _repo;
+
+        public AuthController(IVendorRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(VendorRegisterDto dto)
+        {
+            var existing = await _repo.GetVendorByEmail(dto.Email);
+            if (existing != null)
+            {
+                return BadRequest("Email already Exists");
+            }
+
+            var vendor = new Vendor
+            {
+                BusinessName = dto.BusinessName,
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password)
+            };
+
+            await _repo.RegisterVendor(vendor);
+            return Ok("Vendor registered successfully.");
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(VendorLoginDto dto)
+        {
+            var vendor = await _repo.GetVendorByEmail(dto.Email);
+            if (vendor == null || !BCrypt.Net.BCrypt.Verify(dto.Password, vendor.PasswordHash))
+            {
+                return Unauthorized("Invalid Credentials");
+            }
+
+            return Ok(new { message = "Login Successfull. Token issued by Auth service." });
+
+        }
+
+    }
+}
---
+++ VendorAPI/Migrations/20250408120129_First.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VendorAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Vendors",
+                columns: new[] { "Id", "BusinessName", "Email", "PasswordHash" },
+                values: new object[] { 1, "zzz", "asd@epic.com", "nothing@epic420" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Vendors",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
